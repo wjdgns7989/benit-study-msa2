70aff3ba3ab699a55fcb170a7fb81682009aefe1
김정훈 <wjdgns7989@naver.com>
2023-03-27 02:22:33 +0900

    2023.03.27  - 초기 소스 커밋

---
+++ src/main/java/com/benit/svc2/Svc2Application.java
@@ -0,0 +1,13 @@
+package com.benit.svc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Svc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/benit/svc2/Svc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benit.svc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Svc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
